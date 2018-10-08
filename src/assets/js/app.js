1f3d471d6ad022d5e3fe478b385695be0618d731
robobooga <robobooga@gmail.com>
2018-10-08 21:29:21 +0800

    Added some informative text

--- src/assets/js/app.js
+++ src/assets/js/app.js
@@ -1,5 +1,5 @@
 var clientId = "gcev1q07pomapbs57dl58z2viwec0t";
-var search = "https://api.twitch.tv/kraken/search/streams?query=sg/en&limit=25&offset=0";
+var search = "https://api.twitch.tv/kraken/search/streams?query=sg/en&limit=100&offset=0";
 var encoded = encodeURIComponent(search);
 var streamlist;
 
@@ -13,10 +13,9 @@ $(document).ready(function() {
       'Client-ID': 'gcev1q07pomapbs57dl58z2viwec0t'
       },
       success: function (data) {
-        // $('#blurb').html(JSON.stringify(data));
         streamlist = data.streams;
         for (var i = 0; i < streamlist.length; i++){
-          $('.game').append('<div class="col"><a href="' + streamlist[i].channel.url + '" target="_blank"><article class="card"><img src="' + streamlist[i].preview.medium + '"><div class="card__content"><h2>' + streamlist[i].channel.status + '</h2><p class="streamer"><strong>' + streamlist[i].channel.display_name + '</strong></p><p class="description"><strong>Game: </strong>' + streamlist[i].game + '</p><p class="viewers"><strong>Viewers: </strong>' + streamlist[i].viewers + '</p></article></a></div>');
+          $('.game').append('<div class="col"><a href="' + streamlist[i].channel.url + '" target="_blank"><article class="card"><img src="' + streamlist[i].preview.medium + '"><div class="card__content"><h2>' + streamlist[i].channel.status + '</h2><p class="streamer"><strong>' + streamlist[i].channel.display_name + '</strong></p><p class="description"><i>' + streamlist[i].game + '</i></p><p class="viewers"><strong>Viewers: </strong>' + streamlist[i].viewers + '</p></article></a></div>');
         }
       },
       error: function() {
