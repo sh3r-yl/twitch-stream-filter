0f63cfbe944c06805cd9d3c1c69c3df829db6026
robobooga <robobooga@gmail.com>
2018-10-08 23:39:25 +0800

    Fix title and added ignorelist

--- src/assets/js/app.js
+++ src/assets/js/app.js
@@ -2,6 +2,8 @@ var clientId = "gcev1q07pomapbs57dl58z2viwec0t";
 var search = "https://api.twitch.tv/kraken/search/streams?query=sg/en&limit=100&offset=0";
 var encoded = encodeURIComponent(search);
 var streamlist;
+let ignorelist = new Set();
+ignorelist.add('SG_Batman');
 
 $(document).ready(function() {
   $.ajax({
@@ -15,7 +17,9 @@ $(document).ready(function() {
       success: function (data) {
         streamlist = data.streams;
         for (var i = 0; i < streamlist.length; i++){
-          $('.game').append('<div class="col"><a href="' + streamlist[i].channel.url + '" target="_blank"><article class="card"><img src="' + streamlist[i].preview.medium + '"><div class="card__content"><h2>' + streamlist[i].channel.status + '</h2><p class="streamer"><strong>' + streamlist[i].channel.display_name + '</strong></p><p class="description"><i>' + streamlist[i].game + '</i></p><p class="viewers"><strong>Viewers: </strong>' + streamlist[i].viewers + '</p></article></a></div>');
+          if (!ignorelist.has(streamlist[i].channel.display_name)) {
+            $('.game').append('<div class="col"><a href="' + streamlist[i].channel.url + '" target="_blank"><article class="card"><img src="' + streamlist[i].preview.medium + '"><div class="card__content"><h2>' + streamlist[i].channel.status + '</h2><p class="streamer"><strong>' + streamlist[i].channel.display_name + '</strong></p><p class="description"><i>' + streamlist[i].game + '</i></p><p class="viewers"><strong>Viewers: </strong>' + streamlist[i].viewers + '</p></article></a></div>');
+          }
         }
       },
       error: function() {
