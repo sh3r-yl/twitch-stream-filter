2e8a3d8a26088f6b89ddc1e6897c6f8e7032389b
Sheryl Yeo <sh3r.yl@gmail.com>
2018-10-08 18:18:52 +0800

    Initial commit

---
+++ assets/js/app.js
@@ -0,0 +1,26 @@
+var clientId = "gcev1q07pomapbs57dl58z2viwec0t";
+var search = "https://api.twitch.tv/kraken/search/streams?query=sg/en&limit=25&offset=0";
+var encoded = encodeURIComponent(search);
+var streamlist;
+
+$(document).ready(function() {
+  $.ajax({
+    url: search,
+    type: 'GET',
+    dataType: 'json',
+    headers: {
+      'Accept': 'application/vnd.twitchtv.v5+json',
+      'Client-ID': 'gcev1q07pomapbs57dl58z2viwec0t'
+      },
+      success: function (data) {
+        // $('#blurb').html(JSON.stringify(data));
+        streamlist = data.streams;
+        for (var i = 0; i < streamlist.length; i++){
+          $('.game').append('<div class="col"><a href="' + streamlist[i].channel.url + '" target="_blank"><article class="card"><img src="' + streamlist[i].preview.medium + '"><div class="card__content"><h2>' + streamlist[i].channel.status + '</h2><p class="streamer">' + streamlist[i].channel.display_name + '</p><p class="description"><strong>Game: </strong>' + streamlist[i].game + '</p></article></a></div>');
+        }
+      },
+      error: function() {
+          alert(search);
+      }
+  });
+});
